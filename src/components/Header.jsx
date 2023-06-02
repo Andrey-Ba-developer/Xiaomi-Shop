bb45227c3ded03cad59bdbc086ac74e67682c917
Andrey-Ba-developer <and.balyaeff@yandex.ru>
2023-06-02 14:09:31 +0700

    feat: the main part of the project

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { AppBar, Toolbar, Typography, IconButton, Badge } from "@mui/material";
+import { ShoppingBasket } from "@mui/icons-material";
+
+function Header({ handleCart, orderLen }) {
+	return (
+		<AppBar position="static">
+			<Toolbar>
+				<Typography
+					variant="h6"
+					component="span"
+					sx={{ flexGrow: 1 }}
+				>
+					Xiaomi Shop
+				</Typography>
+				<IconButton
+					color="inherit"
+					onClick={handleCart}
+				>
+					<Badge
+						color="secondary"
+						badgeContent={orderLen}
+					>
+						<ShoppingBasket />
+
+					</Badge>
+				</IconButton>
+			</Toolbar>
+		</AppBar>
+	);
+}
+
+export default Header;
